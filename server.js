4cb3a935f02bd97792d79c127a3fffe3afc95432
pgnox <s23392871@gmail.com>
2026-07-01 16:08:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./db');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to Database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Base Route to check if server is running
+app.get('/', (req, res) => {
+    res.send('RandomChat API is running successfully...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
